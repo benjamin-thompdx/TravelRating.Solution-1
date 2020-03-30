33aa9675b36a72e9c04ce2feabec28291d01e0dc
Katy Solovewicz <katy@solovewicz.com>
2020-03-30 16:33:41 -0700

    add seeded data to second model
    Co-authored-by: Matt Taylor <taylor.matt@protonmail.com>
    Co-authored-by: Benjamin Thom <jaminthom@gmail.com>

---
+++ TravelRating/Migrations/20200330232705_ExperienceSeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelRating.Migrations
+{
+    public partial class ExperienceSeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Experiences",
+                columns: new[] { "ExperienceId", "Author", "LocationId", "Rating", "Review" },
+                values: new object[,]
+                {
+                    { 1, "Ben's Mom", 3, 2, "Hanoi" },
+                    { 2, "Matt's Mom", 3, 2, "Reykjavik" },
+                    { 3, "Katy's Mom", 3, 2, "Kerry" },
+                    { 4, "Katy's Kids' Mom", 3, 2, "New York" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Experiences",
+                keyColumn: "ExperienceId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Experiences",
+                keyColumn: "ExperienceId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Experiences",
+                keyColumn: "ExperienceId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Experiences",
+                keyColumn: "ExperienceId",
+                keyValue: 4);
+        }
+    }
+}
--- TravelRating/Models/TravelRatingContext.cs
+++ TravelRating/Models/TravelRatingContext.cs
@@ -22,6 +22,13 @@ namespace TravelRating.Models
           new Location { LocationId = 3, Name = "Killarney National Park", City = "Kerry", Country = "Ireland"},
           new Location { LocationId = 4, Name = "Clock Walk", City = "New York", Country = "United States"}
         );
+      builder.Entity<Experience>()
+        .HasData(
+          new Experience { ExperienceId = 1, Author = "Ben's Mom", Review = "Hanoi", Rating = 2, LocationId = 3},
+          new Experience { ExperienceId = 2, Author = "Matt's Mom", Review = "Reykjavik", Rating = 2, LocationId = 3},
+          new Experience { ExperienceId = 3, Author = "Katy's Mom", Review = "Kerry", Rating = 2, LocationId = 3},
+          new Experience { ExperienceId = 4, Author = "Katy's Kids' Mom", Review = "New York", Rating = 2, LocationId = 3}
+        );
     }
   }
 }

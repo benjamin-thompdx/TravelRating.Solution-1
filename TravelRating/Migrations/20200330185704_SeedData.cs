347dda5cb62df22be8904db5bcee648d8e3727fa
Benjamin Thom <jaminthom@gmail.com>
2020-03-30 12:01:44 -0700

    add SeedData migration
    Co-authored-by: Matt <taylor.matt@protonmail.com>
    Co-authored-by: K. Wicz <katy@solovewicz.com>

---
+++ TravelRating/Migrations/20200330185704_SeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelRating.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Locations",
+                columns: new[] { "LocationId", "City", "Country", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Hanoi", "Vietnam", "Ha Long Bay" },
+                    { 2, "Reykjavik", "Iceland", "Thingvellir Park" },
+                    { 3, "Kerry", "Ireland", "Killarney National Park" },
+                    { 4, "New York", "United States", "Clock Walk" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Locations",
+                keyColumn: "LocationId",
+                keyValue: 4);
+        }
+    }
+}
--- TravelRating/Models/TravelRatingContext.cs
+++ TravelRating/Models/TravelRatingContext.cs
@@ -12,5 +12,16 @@ namespace TravelRating.Models
 
     public DbSet<Location> Locations { get; set; }
     public DbSet<Experience> Experiences { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Location>()
+        .HasData(
+          new Location { LocationId = 1, Name = "Ha Long Bay", City = "Hanoi", Country = "Vietnam"},
+          new Location { LocationId = 2, Name = "Thingvellir Park", City = "Reykjavik", Country = "Iceland"},
+          new Location { LocationId = 3, Name = "Killarney National Park", City = "Kerry", Country = "Ireland"},
+          new Location { LocationId = 4, Name = "Clock Walk", City = "New York", Country = "United States"}
+        );
+    }
   }
 }
